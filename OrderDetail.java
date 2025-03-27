80534442188e8e4727b229a8cde12377ade2254e
Alex Ratchev <alexratchev3@gmail.com>
2025-03-26 22:11:34 -0400

    Decorator Design Patter GUI
    GUI for customizable pizza. Designed for the decorator design pattern.

--- OrderDetail.java
+++ OrderDetail.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Kazunori Hayashi
 * Version 1.0 29/7/2013
@@ -11,6 +14,11 @@ public class OrderDetail
     private byte quantity;
     private double totalPrice;
 
+    /// @brief
+    /// List of toppings selected for the item (used for Decorator design pattern).
+    /// @author Alex Ratchev
+    private List<String> toppings;
+
     /**
      * Constructor for objects of class OrderDetail
      */
@@ -21,6 +29,9 @@ public class OrderDetail
         this.price      = newMenuItem.getPrice();
         this.quantity   = newQuantity;
         this.totalPrice = this.price * this.quantity;
+
+        //Intialize an empty list to store user-selected toppings
+        this.toppings = new ArrayList<>();
     }
     
     /**************************************************
@@ -46,11 +57,21 @@ public class OrderDetail
     {
         return this.totalPrice;
     }
+
+    /// @brief Gets the list of selected toppings for the order item.
+    /// @return List of topping names.
+    /// @author Alex Ratchev
+    public List<String> getToppings() {return this.toppings;}
     
     public void addQuantity(byte add)
     {
         quantity += add;
         totalPrice = price * quantity;
     }
-        
+    /// @brief Replaces the current toppings with a new list of selected toppings.
+    /// @param newToppings A list of new toppings to set.
+    /// @author Alex Ratchev
+    public void setToppings(List<String> newToppings) {
+        this.toppings = new ArrayList<>(newToppings); // copy for safety
+    }
 }
